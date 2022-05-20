dbadfc47d9056788a825f70a33ef26fcf4906275
genie125 <geniebaby1024@gmail.com>
2022-05-20 14:56:28 +0900

    change style of content and change api key into domain key

--- src/config/constant.tsx
+++ src/config/constant.tsx
@@ -2,7 +2,7 @@ export const API_URL = "https://api.opensea.io/api/v1";
 export const NFTASSETS_API_URL = `${API_URL}/assets`;
 export const NFTASSET_API_URL = `${API_URL}/asset`;
 
-export const apikey = "53ad92b2a106440fa6d8329d945cbb27";
+export const apikey = "c02ab4c73da8426a94efe7800a846d6b";
 export const nftContractAddress = "0x495f947276749Ce646f68AC8c248420045cb7b5e";
 
 export const walletType = {
